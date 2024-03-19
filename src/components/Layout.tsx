0937e98cec05b46409a2965463399c4586f137cc
Higgins723 <xxhiggins723xx@gmail.com>
2024-03-18 23:19:38 -0600

    using built in head to have favicon wrapper vs new package

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import type { HeadFC } from 'gatsby'
 
 type LayoutProps = {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <>
-    <Helmet>
-      <link rel='icon' href='/favicon.ico' type='image/x-icon' />
-    </Helmet>
-
+  <div className='p-4'>
     {children}
-  </>
+  </div>
 )
 
 export default Layout
+
+export const Head: HeadFC = () => <link rel='icon' href='/favicon.ico' type='image/x-icon' />
